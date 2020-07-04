286040d0e3b4668c8f7f28c703aff8e51cbaa367
iChris93 <66568878+iChris93@users.noreply.github.com>
2020-07-04 16:33:11 -0400

    Update GeneralCANSignalAnalysisLibExample.ino

--- examples/GeneralCANSignalAnalysisLibExample.ino
+++ examples/GeneralCANSignalAnalysisLibExample.ino
@@ -10,22 +10,22 @@ Serial.begin(57600);
 void loop() {
   // put your main code here, to run repeatedly:
   //get.Signal inputs from left to right: CANMessage, starting bit (indexed started at 0), singal length in bits, signal scale, signal offset
-int batteryVoltage=analyzeMessage.getSignal(test, 0, 16, 0.01, 0); //analyze "test" CAN message starting at bit 0 with a bit lenght of 16, a scale of 0.01, and an offest of 0
+int batteryVoltage=analyzeMessage.getSignal(test, 0, 16, 0.01, 0, false, 1); //analyze "test" CAN message starting at bit 0 with a bit lenght of 16, a scale of 0.01, and an offest of 0
 Serial.println();
 Serial.print("Battery Votlage: ");
 Serial.print(batteryVoltage);
 Serial.print(" V");
-int batteryCurrent=analyzeMessage.getSignal(test, 16, 16, -0.01, 0);
+int batteryCurrent=analyzeMessage.getSignal(test, 16, 16, -0.01, 0, false, 1);
 Serial.println();
 Serial.print("Battery Current: ");
 Serial.print(batteryCurrent);
 Serial.print(" A");
-int rawBatteryCurrent=analyzeMessage.getSignal(test, 32, 16, -0.05, 500);
+int rawBatteryCurrent=analyzeMessage.getSignal(test, 32, 16, -0.05, 500, false, 1);
 Serial.println();
 Serial.print("Raw Battery Current: ");
 Serial.print(rawBatteryCurrent);
 Serial.print(" A");
-int chargeHoursRemaining=analyzeMessage.getSignal(test, 48, 12, 1, 0);
+int chargeHoursRemaining=analyzeMessage.getSignal(test, 48, 12, 1, 0, false, 1);
 Serial.println();
 Serial.print("Charge Time Remaining: ");
 Serial.print(chargeHoursRemaining);
